534f2469a3a3e550f5be4b3f5506e9d383eab4c8
ianoxwell <ianoxwell@gmail.com>
2021-07-26 17:09:58 +1000

    image updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
 	production: true,
 	appTitle: 'Angular Material WCAG demonstration',
 	baseURL: 'http://localhost:4200/',
+	baseTitle: '',
 	apiUrl: 'https://localhost:44302/api',
 	resultsPerPage: 15,
 	defaultRoute: '/'
